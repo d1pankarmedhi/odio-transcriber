bda2de0cce1033e13357f884497840de534811e6
d1pankarmedhi <dipankarmedhi11@gmail.com>
2023-12-09 14:32:46 +0530

    chore: whisper code refactor

--- transcribe_service/main.py
+++ transcribe_service/main.py
@@ -1,13 +1,16 @@
-import whisper
 from fastapi import FastAPI, UploadFile, File
 import os 
 import uvicorn
-
+import os
+from whisper import Whisper
 
 app = FastAPI()
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+    
+
 
 @app.get("/")
 async def home():
@@ -16,21 +19,25 @@ async def home():
 @app.post("/transcribe/audio")
 async def transcribe(file: UploadFile = File(...)):
 
+
     file_path = f"{UPLOAD_DIR}/{file.filename}"
     with open(file_path, "wb") as f:
         f.write(file.file.read())
 
     print(f"Loading model..")
-    whisper_model = whisper.load_model("models/base.pt")
+    whisper = Whisper()
+    pipe = whisper.pipeline()
     print(f"Model loaded...")
 
-    result = whisper_model.transcribe(file_path)
+    result = pipe(file_path)
     # print(result['text'])
 
     # delete the saved audio file
     os.remove(file_path)
 
-    return {"Message": file.filename, "Text": result['text']}
+    return {"status": 200, "Message": file.filename, "Text": result['text'], "model": "_".join(str(whisper.model_id).split('-')) }
+
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
